67fa09ad30033d7d21d3568f69d0227bd807c4be
chupaYchups <chupaYchups@gmail.com>
2020-01-14 08:05:09 +0300

    Добавил шапку с описанием команд сборки, разархивирования, запуска

--- l01.1-maven/src/main/java/ru/otus/l011/HelloOtus.java
+++ l01.1-maven/src/main/java/ru/otus/l011/HelloOtus.java
@@ -2,6 +2,25 @@ package ru.otus.l011;
 
 import com.google.common.primitives.Floats;
 
+/**
+ * Example for L01.1
+ *
+ * To start the application:
+ * mvn package
+ * java -jar ./l01.1-maven/target/l01.1-maven-jar-with-dependencies.jar
+ * java -cp "./l01.1-maven/target/L01.1-maven.jar:${HOME}/.m2/repository/com/google/guava/guava/27.1-jre/guava-27.1-jre.jar" ru.otus.l011.Main
+ *
+ * To unzip the jar:
+ * unzip -l L01-maven.jar
+ * unzip -l L01-maven-jar-with-dependencies.jar
+ *
+ * To build:
+ * mvn package
+ * mvn clean compile
+ * mvn assembly:single
+ * mvn clean compile assembly:single
+ */
+
 public class HelloOtus {
     public static void main(String[] args) {
         float f1 = 1.1f;
