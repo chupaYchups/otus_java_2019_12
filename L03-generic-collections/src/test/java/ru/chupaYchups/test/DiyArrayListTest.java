e3c8f591a56cb7bf6c85cd945af79e5a63762227
chupaYchups <chupaYchups@gmail.com>
2020-01-15 10:59:26 +0300

    new module for 3 homework, add list class and testing structure

---
+++ L03-generic-collections/src/test/java/ru/chupaYchups/test/DiyArrayListTest.java
@@ -0,0 +1,21 @@
+package ru.chupaYchups.test;
+
+import org.junit.Test;
+
+/**
+ * Тест работоспособности реализации списка DiyArrayList
+ */
+public class DiyArrayListTest {
+
+    @Test
+    public void testAddAll() {
+    }
+
+    @Test
+    public void testSort() {
+    }
+
+    @Test
+    public void testCopy() {
+    }
+}
