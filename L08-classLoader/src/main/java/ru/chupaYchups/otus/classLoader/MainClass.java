c141b9a4693ea9f02fa89bf88c01cb29c966e6b0
chupaYchups <chupaYchups@gmail.com>
2020-02-11 13:27:57 +0300

    homework 6

---
+++ L08-classLoader/src/main/java/ru/chupaYchups/otus/classLoader/MainClass.java
@@ -0,0 +1,10 @@
+package ru.chupaYchups.otus.classLoader;
+import ru.chupaYchups.otus.classLoader.proxy.IoC;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ITestLogging testLogging = IoC.createLoggingProxy();
+        testLogging.calculation(6);
+        testLogging.calculationNotLogging(6);
+    }
+}
