d467806718ad1c1f7ac9c13b7e3b3bc5ea69c118
chupaYchups <chupaYchups@gmail.com>
2020-02-25 15:20:11 +0300

    homework 11

--- L11-solid/src/main/java/ru/chupaYchups/atm/bill/BillNominal.java
+++ L11-solid/src/main/java/ru/chupaYchups/atm/bill/BillNominal.java
@@ -1,5 +1,14 @@
 package ru.chupaYchups.atm.bill;
 
 public enum BillNominal {
-    NOMINAL_50, NOMINAL_100, NOMINAL_1000;
+
+    NOMINAL_50 (50),
+    NOMINAL_100(100),
+    NOMINAL_1000(1000);
+
+    BillNominal(int nominal) {
+        this.nominal = nominal;
+    }
+
+    private int nominal;
 }
