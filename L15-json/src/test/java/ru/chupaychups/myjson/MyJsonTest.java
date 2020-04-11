d627320957226b7672cd9b47966c7c4921656b60
chupaYchups <chupaYchups@gmail.com>
2020-04-11 11:16:38 +0300

    home work 15

--- L15-json/src/test/java/ru/chupaychups/myjson/MyJsonTest.java
+++ L15-json/src/test/java/ru/chupaychups/myjson/MyJsonTest.java
@@ -3,18 +3,48 @@ package ru.chupaychups.myjson;
 import com.google.gson.Gson;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
 
 @DisplayName("Тест проверяющий что моя реализация JSON")
 class MyJsonTest {
 
-    @DisplayName("Корректно преобразует разные типы обьектов в строку")
+    private TestJsonClass createTestObject() {
+        Random random = new Random();
+        int[] intArray = new int[]{random.nextInt(),  random.nextInt(),  random.nextInt()};
+        return new TestJsonClass(random.nextLong() + "_TEST",
+        random.nextInt(), intArray);
+    }
+
+    @Test
+    @DisplayName("Корректно преобразует произвольный обьект в строку")
+    void myJsonCorrectlyTransformCustomObjectToString() {
+        TestJsonClass testJsonClass = createTestObject();
+        Assertions.assertThat(new MyJson().toJson(testJsonClass)).isEqualTo(new Gson().toJson(testJsonClass));
+    }
+
+    @Test
+    @DisplayName("Корректно преобразует коллекцию произвольных обьектов в строку")
+    void myJsonCorrectlyTransformCollectionOfCustomObjectsToString() {
+        var myObjectList = List.of(createTestObject(), createTestObject(), createTestObject());
+        Assertions.assertThat(new MyJson().toJson(myObjectList)).isEqualTo(new Gson().toJson(myObjectList));
+    }
+
+    @Test
+    @DisplayName("Корректно преобразует масив произвольных обьектов в строку")
+    void myJsonCorrectlyTransformArrayOfCustomObjectsToString() {
+        var myObjectArray = new TestJsonClass[] {createTestObject(), createTestObject(), createTestObject()};
+        Assertions.assertThat(new MyJson().toJson(myObjectArray)).isEqualTo(new Gson().toJson(myObjectArray));
+    }
+
+    @DisplayName("Корректно преобразует различные вариации входных параметров, с примитивными типами, в строку")
     @ParameterizedTest
     @MethodSource("generateDataForCustomTest")
     void customTest(Object o){
