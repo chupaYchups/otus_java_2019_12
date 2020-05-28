f151f7b14212f9c301279f47881b09c5602d032a
chupaYchups <chupaYchups@gmail.com>
2020-05-28 20:51:07 +0300

    home work 19

---
+++ L19-hibernate/src/main/java/com/chupaYchups/hibernate/model/AddressDataSet.java
@@ -0,0 +1,14 @@
+package com.chupaYchups.hibernate.model;
+
+public class AddressDataSet {
+
+    private String street;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
---
+++ L19-hibernate/src/main/java/com/chupaYchups/hibernate/model/PhoneDataSet.java
@@ -0,0 +1,14 @@
+package com.chupaYchups.hibernate.model;
+
+public class PhoneDataSet {
+
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
